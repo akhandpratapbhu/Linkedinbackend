70767abbd50d1d99d6c24f275dfdc9b5529467c2
Akhand Pratap <akhand.pratap@netsmartz.local>
2024-06-06 13:41:33 +0530

    chat functionality

--- src/chat/gateway/chat/chat.gateway.ts
+++ src/chat/gateway/chat/chat.gateway.ts
@@ -1,74 +1,73 @@
 
-// import { WebSocketGateway, WebSocketServer, SubscribeMessage, OnGatewayConnection, OnGatewayDisconnect } from '@nestjs/websockets';
-// import { Server, Socket } from 'socket.io';
 
-// @WebSocketGateway()
-// export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
-
-//     @WebSocketServer() server: Server;
-
-//     handleConnection(socket: Socket) {
-//         console.log('Client connected: ' + socket.id);
-//     }
-
-//     handleDisconnect(socket: Socket) {
-//         console.log('Client disconnected: ' + socket.id);
-//     }
-
-//     @SubscribeMessage('join')
-//     handleJoin(client: Socket, data: { room: string }) {
-//         client.join(data.room);
-//         client.broadcast.to(data.room).emit('user joined');
-//     }
-
-//     @SubscribeMessage('message')
-//     handleMessage(client: Socket, data: { room: string, user: string, message: string }) {
-//         this.server.to(data.room).emit('new message', { user: data.user, message: data.message });
-//     }
-// }
-
-
-import {
-    WebSocketGateway,
-    SubscribeMessage,
-    MessageBody,
-    ConnectedSocket,
-    OnGatewayInit,
-    OnGatewayConnection,
-    OnGatewayDisconnect,
-    WebSocketServer,
-  } from '@nestjs/websockets';
-  import { ChatService } from './chat.service';
-  import { Socket } from 'socket.io';
-import { Server } from 'http';
+// import {
+//     WebSocketGateway,
+//     SubscribeMessage,
+//     MessageBody,
+//     ConnectedSocket,
+//     OnGatewayInit,
+//     OnGatewayConnection,
+//     OnGatewayDisconnect,
+//     WebSocketServer,
+//   } from '@nestjs/websockets';
+//   import { ChatService } from './chat.service';
+//   import { Socket } from 'socket.io';
+// import { Server } from 'http';
   
-  @WebSocketGateway()
-  export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
-    constructor(private readonly chatService: ChatService) {}
+//   @WebSocketGateway()
+//   export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+//     constructor(private readonly chatService: ChatService) {}
   
-    @WebSocketServer()
-    server:Server
+//     @WebSocketServer()
+//     server:Server
 
-    @SubscribeMessage('sendMessage')
-    async handleMessage(@MessageBody() message: any, @ConnectedSocket() client: Socket): Promise<void> {
+//     @SubscribeMessage('sendMessage')
+//     async handleMessage(@MessageBody() message: any, @ConnectedSocket() client: Socket): Promise<void> {
      
       
-      const savedMessage = await this.chatService.createMessage(message);
-      console.log(savedMessage);
-      this.server.emit('receiveMessage',savedMessage)
-      //client.to(message.recipientId).emit('receiveMessage', savedMessage);
-    }
+//       const savedMessage = await this.chatService.createMessage(message);
+//       console.log(savedMessage);
+//       this.server.emit('receiveMessage',savedMessage)
+//       //client.to(message.recipientId).emit('receiveMessage', savedMessage);
+//     }
   
-    afterInit(server: any) {
-      console.log('Init');
-    }
+//     afterInit(server: any) {
+//       console.log('Init');
+//     }
   
-    handleDisconnect(client: Socket) {
-      console.log(`Client disconnected: ${client.id}`);
-    }
+//     handleDisconnect(client: Socket) {
+//       console.log(`Client disconnected: ${client.id}`);
+//     }
   
-    handleConnection(client: Socket, ...args: any[]) {
-      console.log(`Client connected: ${client.id}`);
-    }
+//     handleConnection(client: Socket, ...args: any[]) {
+//       console.log(`Client connected: ${client.id}`);
+//     }
+//   }
+import { WebSocketGateway, WebSocketServer, SubscribeMessage, MessageBody, ConnectedSocket, OnGatewayConnection, OnGatewayDisconnect } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway()
+export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer()
+  server: Server;
+
+  handleConnection(client: Socket) {
+    console.log('a user connected:', client.id);
   }
-  
+
+  handleDisconnect(client: Socket) {
+    console.log('user disconnected:', client.id);
+  }
+
+  @SubscribeMessage('joinRoom')
+  handleJoinRoom(@MessageBody() data: { roomId: string }, @ConnectedSocket() client: Socket) {
+    client.join(data.roomId);
+    console.log(`User joined room: ${data.roomId}`);
+  }
+
+  @SubscribeMessage('sendMessage')
+  handleMessage(@MessageBody() data: { roomId: string; message: any }, @ConnectedSocket() client: Socket) {
+    this.server.to(data.roomId).emit('message', data.message);
+    console.log(`Message sent to room ${data.roomId}: ${data.message}`);
+  }
+}
